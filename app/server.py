ef8f7a9f69e1b1a582a64b932fe801b8a143bf06
GaMeNu <98153342+GaMeNu@users.noreply.github.com>
2025-06-04 14:48:10 +0300

    .env for pgsql conn
    Added using .env file for database connection details.
    
    .env file format:
    PORT=
    
    DB_USERNAME=
    DB_PASSWORD=
    DB_NAME=

--- app/server.py
+++ app/server.py
@@ -76,4 +76,4 @@ def runSQL(sql_string):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # create tables
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=os.getenv("PORT"), debug=True)
