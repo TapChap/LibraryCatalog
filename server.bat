90fd4ed65eb48cac9e49dabeb72ea4eaaccf76be
Your Name <you@example.com>
2025-07-07 18:42:43 +0300

    update server.bat

--- server.bat
+++ server.bat
@@ -3,10 +3,4 @@ cd "app"
 echo Starting update listener...
 start "" pythonw CICD.pyw > CICD.log 2>&1
 
-echo Starting server app...
-start "" pythonw server.pyw > server.log 2>&1
-
-echo All started in background.
 exit
-
-REM TASKKILL /F /IM pythonw.exe
