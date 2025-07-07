d277153459ba9c45ecc92f9653bf754c7a437311
Your Name <you@example.com>
2025-07-07 17:31:31 +0300

    update server.bat to prod env

--- server.bat
+++ server.bat
@@ -1,5 +1,4 @@
-@echo off
-cd "./app"
+cd "%projectpath%\app"
 
 echo Starting update listener...
 start "" pythonw CICD.pyw > CICD.log 2>&1
@@ -8,6 +7,6 @@ echo Starting server app...
 start "" pythonw server.pyw > server.log 2>&1
 
 echo All started in background.
-exit
+@REM exit
 
 REM TASKKILL /F /IM pythonw.exe
